36973a37d3cbe591678c37fe70f836186f1b0229
karina heras <karinaheras@MacBook-Pro-de-karina.local>
2020-05-16 11:48:41 +0200

    cambios

--- src/resources/users/model.js
+++ src/resources/users/model.js
@@ -1,44 +1,23 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import uniqueValidator from 'mongoose-unique-validator';
+import RoleEnum from './enum.roles';
 
-let rolesValidos = {
-values: ["ADMIN", "PUBLISHER"],
-message: '{VALUE} no es un role válido'
-}
-let Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-let usuarioSchema = new Schema({
-nombre: {
-type: String,
-required: [true, 'El nombre es necesario'],
-},
-email: {
-type: String,
-unique: true,
-required: [true, "El correo es necesario"],
-},
-password: {
-type: String,
-required: [true, "Le contraseña es obligatoria"],
-},
-role: {
-type: String,
-default: 'ADMIN',
-required: [true],
-enum: rolesValidos,
-},
+const UserSchema = new Schema({
+    username: { type: 'String' },
+    password: { type: 'String' },
+    role: { type: 'String', enum: [RoleEnum.ROLES.ADMIN, RoleEnum.ROLES.PUBLISHER] }
+}, { collection: 'users' });
+
+UserSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
-
-// elimina la key password del objeto que retorna al momento de crear un usuario
-usuarioSchema.methods.toJSON = function() {
-let user = this;
-let userObject = user.toObject();
-delete userObject.password;
-return userObject;
+UserSchema.methods.isValidPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
 }
 
-usuarioSchema.plugin(uniqueValidator, {
-message: '{PATH} debe de ser único'
-})
-export default mongoose.model('Users', usuarioSchema)
+
+export default mongoose.model('user', UserSchema);
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import CommentController from './resources/comments/controller';
 import OffensiveWordController from './resources/offensivewords/controller';
 import PostController from './resources/post/controller';
 import UsersController from './resources/users/controller';
-
+const cors = require('cors');
 
 export default app => {
+    app.use(cors());
     app.use('/posts', PostController);
     app.use('/comments', CommentController);
     app.use('/offensivewords', OffensiveWordController);
     app.use('/auth', AuthController);
     app.use('/users', UsersController);
+  
 }
 
